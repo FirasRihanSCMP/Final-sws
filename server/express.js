b4c1fea849ab2b05a9c7962514734bf0c4be80a0
FirasRihanSCMP <firas.rihan@scmp-lb.com>
2022-02-07 10:54:52 +0200

    version 1.0.1

--- server/express.js
+++ server/express.js
@@ -55,7 +55,7 @@ app.use(express.json());
 
 
 
-app.use(express.static(path.resolve(__dirname, '../', 'build')))
+app.use(express.static(path.resolve(__dirname, '../client', 'build')))
 
 app.get("/api/Events", async (req, res) => {
   try {
